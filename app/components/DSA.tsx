39d9094e2e06c9ee5728ed81defd12b93b80ccd8
Shamanbenny <E1186165@u.nus.edu>
2024-05-26 02:20:46 +0800

    Updated DSA.tsx with temporary Commit Snake

--- app/components/DSA.tsx
+++ app/components/DSA.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 const DSA = () => {
@@ -30,13 +31,42 @@ const DSA = () => {
             dark:bg-neutral-900 dark:text-neutral-300 lg:pl-[80px]`
         }
       >
-        <div className="w-full text-center">
-          <h1 className="pt-5 text-[4rem] text-emerald-700 drop-shadow-[0_0_6px] dark:text-emerald-500 max-sm:text-[3rem]">
-            /dsa
-          </h1>
-        </div>
-        <div>
-          <p>Test</p>
+        <div
+          className="mx-auto max-sm:w-[300px] max-xs:w-[230px] sm:w-[560px] md:w-[680px] lg:w-[910px] 
+              xl:w-[1160px] xxl:w-[1480px]"
+        >
+          <div className="w-full text-center">
+            <h1 className="pt-5 text-[4rem] text-emerald-700 drop-shadow-[0_0_6px] dark:text-emerald-500 max-sm:text-[3rem]">
+              /dsa
+            </h1>
+          </div>
+
+          {/* [START] Commit Snake*/}
+          <div className="w-full">
+            <h1
+              className="z-[6] mx-auto mb-3 w-[90%] border-b-2 border-neutral-900 pt-5 text-left text-[1.4rem] dark:border-neutral-300 
+                max-lg:pt-3 lg:text-[1.8rem] xl:mb-5 xl:text-[2rem] xxl:text-[2.4rem]"
+            >
+              Commit Snake
+            </h1>
+            <Image
+              src="/commitSnake/snake.svg"
+              alt="Commit Snake"
+              width={880}
+              height={192}
+              className="mx-auto block h-auto w-[90%] max-w-[1200px] dark:hidden"
+            />
+            <Image
+              src="/commitSnake/snake-dark.svg"
+              alt="Commit Snake"
+              width={880}
+              height={192}
+              className="mx-auto hidden h-auto w-[90%] max-w-[1200px] dark:block"
+            />
+          </div>
+          {/* [END] Commit Snake*/}
+
+          {/* DSA Content - Repeated Components */}
         </div>
       </div>
     </>
